bae0d9f9df4fa384f9fd67290784905aaf41f57e
nojrux <nojrux@gmail.com>
2025-12-03 21:48:33 +0200

    A VERY VERY small, but VERY VERY important patch

--- ai.py
+++ ai.py
@@ -1,7 +1,10 @@
 from openai import OpenAI
+import os
+
+os.getenv("MY_SECRET")
 
 client = OpenAI(
-    api_key="AIzaSyCQrt4xfUYePAR38_6d-qFHTVqpvcoHIh8",
+    api_key=MY_SECRET,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
