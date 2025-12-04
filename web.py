de1d917c619e013e2bb555cfb66e746428367f7f
NojRux <nojrux@gmail.com>
2025-12-04 13:22:51 +0200

    Update web.py

--- web.py
+++ web.py
@@ -82,5 +82,5 @@ def chat():
     return jsonify({"response": ai_response})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
